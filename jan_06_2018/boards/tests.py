1860d64083725d2a4e371e8eafada38647fb0f35
JC-ITData <judy.cazares@protonmail.com>
2019-02-03 14:47:06 -0600

    add validation errors testing, add test_contains_form

--- jan_06_2018/boards/tests.py
+++ jan_06_2018/boards/tests.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from .views import home, board_topics, new_topic
 from .models import Board, Topic, Post
 from django.contrib.auth.models import User
+from .forms import NewTopicForm
 
 class HomeTests(TestCase):
     def setUp(self):
@@ -91,14 +92,16 @@ class NewTopicTests(TestCase):
         response = self.client.post(url, data)
         self.assertTrue(Topic.objects.exists())
         self.assertTrue(Post.objects.exists())
-    def test_new_topic_invalid_post_date(self):
+    def test_new_topic_invalid_post_data(self):
         '''
         invalid post data should not redirect
         the expected behavior is to show the form again with validation errors
         '''
         url = reverse('new_topic', kwargs={'pk': 1})
         response = self.client.post(url, {})
+        form = response.context.get('form')
         self.assertEquals(response.status_code, 200)
+        self.assertTrue(form.errors)
     def test_new_topic_invalid_post_date_empty_fields(self):
         '''
         invalid post data should not redirect
@@ -113,3 +116,8 @@ class NewTopicTests(TestCase):
         self.assertEquals(response.status_code, 200)
         self.assertFalse(Topic.objects.exists())
         self.assertFalse(Post.objects.exists())
+    def test_contains_form(self):
+        url = reverse('new_topic', kwargs={'pk': 1})
+        response = self.client.get(url)
+        form = response.context.get('form')
+        self.assertIsInstance(form, NewTopicForm)
