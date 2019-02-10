6be7b9f706b82afa9391fd4d117a5c37f92139fb
JC-ITData <judy.cazares@protonmail.com>
2019-02-10 13:19:43 -0600

    update signup page to use UserCreationForm

--- jan_06_2018/accounts/views.py
+++ jan_06_2018/accounts/views.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 
 def signup(request):
+    form = UserCreationForm()
     return render(request, 'signup.html')
