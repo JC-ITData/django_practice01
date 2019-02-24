5950b4fbd55d38b8a8c0453a960a50d2e1f72105
JC-ITData <judy.cazares@protonmail.com>
2019-02-24 12:31:35 -0600

    password reset complete template and tests

--- jan_06_2018/accounts/tests/test_view_password_reset.py
+++ jan_06_2018/accounts/tests/test_view_password_reset.py
@@ -139,3 +139,12 @@ class InvalidPasswordResetConfirmTests(TestCase):
         password_reset_url = reverse('password_reset')
         self.assertContains(self.response, 'invalid password reset link')
         self.assertContains(self.response, 'href="{0}"'.format(password_reset_url))
+
+class PasswordResetCompleteTests(TestCase):
+    def setUp(self):
+        url = reverse('password_reset_complete')
+        self.response = self.client.get(url)
+
+    def test_status_code(self):
+        view = resolve('/reset/complete/')
+        self.assertEquals(view.func.view_class, auth_views.PasswordResetCompleteView)
