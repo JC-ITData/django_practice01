a343f32bb273e284d4685f12b9160eac3f0efc15
JC-ITData <judy.cazares@protonmail.com>
2019-03-31 13:29:39 -0500

    Add import count

--- jan_06_2018/boards/views.py
+++ jan_06_2018/boards/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, get_object_or_404
