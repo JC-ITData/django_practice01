78d47e0530b5b8c7c8a291d1b58bcde1439537d1
JC-ITData <judy.cazares@protonmail.com>
2019-03-31 12:36:36 -0500

    add import truncator

--- jan_06_2018/boards/models.py
+++ jan_06_2018/boards/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import Truncator
 
 class Board(models.Model):
     name = models.CharField(max_length=30, unique=True)
